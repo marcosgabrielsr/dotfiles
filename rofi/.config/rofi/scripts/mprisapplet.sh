f592870414921b06af002e11536103f4d9670af6
marcosgabrielsr <marcosdevse@gmail.com>
2025-12-14 12:40:07 -0400

    feat: Add title size controller

--- rofi/.config/rofi/scripts/mprisapplet.sh
+++ rofi/.config/rofi/scripts/mprisapplet.sh
@@ -6,9 +6,16 @@ if [ -z $1 ]; then
 fi
 
 selected_player="$1"
+
 status=$(playerctl -p "$selected_player" status 2>/dev/null)
-title=$(playerctl -p "$p_id" metadata xesam:title 2>/dev/null)
-artist=$(playerctl -p "$p_id" metadata xesam:artist 2>/dev/null)
+title=$(playerctl -p "$selected_player" metadata xesam:title 2>/dev/null)
+artist=$(playerctl -p "$selected_player" metadata xesam:artist 2>/dev/null)
+
+if [ ${#title} -gt 45 ]; then
+    final_title="${title:0:45}..."
+else
+    final_title="$title"
+fi
 
 toggle=""
 if [ "$status" = "Playing" ]; then
@@ -18,11 +25,11 @@ fi
 prev=""
 next=""
 
-display_text="<span weight='light' size='small' alpha='50%'>${selected_player} ${status}</span>\n\n${title}\n<span size='small' style='italic' alpha='65%'>${artist}</span>"
+display_title="<span weight='light' size='small' alpha='40%'>${selected_player} ${status}</span>"
+display_song="<span size='85%'>${final_title}</span>\n<span size='small' style='italic' alpha='60%'>${artist}</span>"
 
-
-selected_option=$(echo -e "$prev\n$toggle\n$next" | rofi -dmenu -theme "$player_applet" -theme-str "textbox-custom { str: \"$display_text\";}" -select "$toggle")
-[ -z "$selected_option" ] && exit 0
+selected_option=$(echo -e "$prev\n$toggle\n$next" | rofi -dmenu -theme "$player_applet" -theme-str "textbox-title { str: \"$display_title\";}" -theme-str "textbox-song { str: \"$display_song\";}" -select "$toggle")
+[ -z "$selected_option"] && exit 0
 
 case "$selected_option" in
     "$prev")    playerctl previous ;;
